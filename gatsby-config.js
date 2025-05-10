1072e3172b6e0aeee1200fd07313250312cad480
bohdan-koldun <koldunbohdan@gmail.com>
2025-05-11 00:09:28 +0300

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,14 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
